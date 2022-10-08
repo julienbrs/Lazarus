adee64a8c79b24cff42f548f3fda7bec32697a4f
julienbrs <106234742+julienbrs@users.noreply.github.com>
2022-10-09 00:55:38 +0200

    transitions done ?
    Transitions of text section, mp4 statue (without blur)

---
+++ Lazarus/app.js
@@ -0,0 +1,16 @@
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add('visible');
+        } else {
+            entry.target.classList.remove('visible');
+        }
+    });
+});
+
+
+const hiddenElements_left = document.querySelectorAll('.hidden_left');
+const hiddenElements_right = document.querySelectorAll('.hidden_right');
+
+hiddenElements_left.forEach((element) => {observer.observe(element);});
+hiddenElements_right.forEach((element) => {observer.observe(element);});
